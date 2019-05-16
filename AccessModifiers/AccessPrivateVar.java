45701666e7b1c0417b0ccee9de215181e8bfc8d2
GANESH0080 <39731413+GANESH0080@users.noreply.github.com>
2019-05-16 13:37:22 +0530

    Update AccessPrivateVar.java
    Example of how to access private Variables.

--- AccessModifiers/AccessPrivateVar.java
+++ AccessModifiers/AccessPrivateVar.java
@@ -1,1 +1,13 @@
-Example of how to access private Variables.
+
+package AccessModifiers;
+
+public class AccessPrivateVar {
+
+	public static void main(String[] args) {
+		PrivateVar test = new PrivateVar();
+		test.setName("Ganesh");
+
+		System.out.println(test.getName());
+	}
+
+}
