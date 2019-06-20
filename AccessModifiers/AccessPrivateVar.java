28caf864ab029fde613b16298c968246da2106d5
GANESH0080 <39731413+GANESH0080@users.noreply.github.com>
2019-06-20 16:23:10 +0530

    Update AccessPrivateVar.java

--- AccessModifiers/AccessPrivateVar.java
+++ AccessModifiers/AccessPrivateVar.java
@@ -5,7 +5,7 @@ public class AccessPrivateVar {
 
 	public static void main(String[] args) {
 		PrivateVar test = new PrivateVar();
-		test.setName("Ganesh");
+		test.setName("Ganesh Salunkhe");
 
 		System.out.println(test.getName());
 	}
